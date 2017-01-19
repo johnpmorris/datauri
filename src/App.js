4bb5d2a46f19deae7d7d5755ba57e8bbc673678f
John Morris <johnmorris1095@gmail.com>
2017-01-19 22:10:26 +1000

    add settings

--- src/App.js
+++ src/App.js
@@ -1,53 +1,145 @@
 import React, { Component } from 'react';
 
+/* eslint-disable */
+
 export default class App extends Component {
   constructor(){
     super();
-    this.encodeAndUri = this.encodeAndUri.bind(this)
-    this.encode = this.encode.bind(this)
-    this.dataUri = this.dataUri.bind(this)
+    this.state = {
+      textVal: '',
+      uri: true,
+      uriType: 'bg',
+      encode: true
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.encodeToggle = this.encodeToggle.bind(this)
+    this.uriToggle = this.uriToggle.bind(this)
+    this.uriTypeToggle = this.uriTypeToggle.bind(this)
   }
 
-  encode(){
-    const obj = this.refs.encodeInput;
-    const unencoded = obj.value;
-    obj.value = encodeURIComponent(unencoded).replace(/'/g,"%27").replace(/"/g,"%22");
+  encode(val){
+    return  encodeURIComponent(val).replace(/'/g,"%27").replace(/"/g,"%22");
   }
 
-  dataUri() {
-    const obj = this.refs.encodeInput;
-    const inputValue = obj.value;
-    obj.value = `background-image: url('data:image/svg+xml;utf8,${inputValue}');`
+  dataUri(val) {
+    const { uriType } = this.state
+    if (uriType === 'bg') {
+      return `background-image: url('data:image/svg+xml;utf8,${val}');`
+    } else if (uriType === 'img') {
+      return `<img src='data:image/svg+xml;utf8,${val}'>`
+    }
   }
 
-  encodeAndUri() {
-    this.encode()
-    this.dataUri()
+  encodeToggle() {
+    this.setState({
+      encode: !this.state.encode
+    })
+  }
+
+  uriToggle() {
+    this.setState({
+      uri: !this.state.uri
+    })
+  }
+
+  uriTypeToggle() {
+    const { uriType } = this.state
+    this.setState({
+      uriType: (uriType === 'bg' ? 'img' : 'bg')
+    })
+  }
+
+  handleClick() {
+    const input = this.refs.textArea
+    const val = input.value
+    const encodedVal = this.state.encode ? this.encode(val) : val
+    const uri = this.state.uri ? this.dataUri(encodedVal) : encodedVal
+    this.setState({
+      textVal: uri
+    })
+    input.value = uri
+  }
+
+  componentDidUpdate(){
+    this.refs.textArea.focus()
+  }
+
+  getButtonText(){
+    const { uri, encode, uriType } = this.state
+    if (uri) {
+      return `Create ${encode ? 'encoded' : ''} uri`
+    } else if (!uri) {
+      return encode ? 'Encode' : 'Do nothing...'
+    }
   }
 
   render() {
+    const { uri, uriType, encode } = this.state;
     return (
       <div style={styles.app}>
+        {this.renderHeader()}
         <textarea
+          autoFocus
           autoComplete="off"
           autoCorrect="off"
           autoCapitalize="off"
           spellCheck="false"
           style={styles.textArea}
-          ref="encodeInput"
+          ref="textArea"
         />
         <div style={styles.buttonWrapper}>
-          <div style={styles.button} onClick={this.encode}>Encode</div>
-          <div style={styles.button} onClick={this.dataUri}>data uri</div>
-          <div style={styles.button} onClick={this.encodeAndUri}>encode and data uri</div>
+          <div style={{display: 'flex'}}>
+            <div style={{...styles.button}} onClick={this.encodeToggle}>
+              {encode ? 'Encode' : "Don't encode"}
+            </div>
+            <div style={{...styles.button}} onClick={this.uriToggle}>
+              {uri ? 'create uri' : "don't create uri"}
+            </div>
+            {uri &&
+              <div style={{...styles.button, background: '#333', color: 'white'}} onClick={this.uriTypeToggle}>
+                uri type: {uriType === 'bg' ? 'background-image' : "<img>"}
+              </div>
+            }
+          </div>
+          <div
+            style={{...styles.button, background: 'blue', color: 'white'}}
+            onClick={this.handleClick}>
+            {this.getButtonText()}
+          </div>
         </div>
       </div>
     );
+  }
+
+  renderHeader() {
+    return(
+      <header style={{padding: '0.5em 2em'}}>
+        <h1 style={{fontSize: '1.5em'}}>URL encoder / SVG Data Uri Generator</h1>
+        <p>> Why?<br/>
+          <a
+            target="_blank"
+            style={{color: 'white'}}
+            href="https://css-tricks.com/probably-dont-base64-svg/">
+            Saves a request, no assets to maintain.
+          </a><br/>
+          > Ok, so why turn it into gibberish? <br/>
+          <a
+            target="_blank"
+            style={{color: 'white'}}
+            href="http://stackoverflow.com/questions/7122760/data-uri-svg-background-in-css-not-working-in-firefox">
+            firefox complains sometimes.
+          </a>
+        </p>
+      </header>
+    )
   }
 }
 
 const styles = {
   app: {
+    lineHeight: '1.3em',
+    background: 'black',
+    color: 'white',
     position: 'absolute',
     top: 0, right: 0, bottom: 0, left: 0,
     overflow: 'hidden'
@@ -55,7 +147,7 @@ const styles = {
 
   textArea: {
     fontSize: '1em',
-    padding: '2em',
+    padding: '0 2em',
     boxSizing: 'border-box',
     display: 'block',
     appearance: 'none',
@@ -63,7 +155,7 @@ const styles = {
     background: 'black',
     color: 'white',
     width: '100vw',
-    height: '100vh',
+    height: 'calc(100vh - 15em)',
     position: 'absolute',
     margin: 0,
     outline: 'none',
@@ -73,13 +165,17 @@ const styles = {
   buttonWrapper: {
     position: 'absolute',
     right: '1.5em',
+    left: '1.5em',
     bottom: '1em',
     display: 'flex',
+    justifyContent: 'space-between',
     paddingTop: '0.2em'
   },
 
   button: {
+    userSelect: 'none',
     background: 'white',
+    color: 'black',
     cursor: 'pointer',
     padding: '0.5em',
     marginLeft: '0.25em',
